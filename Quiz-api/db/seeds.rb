da79e4c537db7be3df15f14d6a7dc8687d73eee2
github username <github email address>
2020-02-02 17:41:38 -0500

    added backend

---
+++ Quiz-api/db/seeds.rb
@@ -0,0 +1,121 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+q1= Question.create(text: "Why does JavaScript and Java have similar names?")
+q2= Question.create(text: "")
+q3= Question.create(text: "")
+q4= Question.create(text: "")
+q5= Question.create(text: "")
+q6= Question.create(text: "")
+q7= Question.create(text: "")
+q8= Question.create(text: "")
+q9= Question.create(text: "")
+q10= Question.create(text: "")
+q11= Question.create(text: "")
+q12= Question.create(text: "")
+q13= Question.create(text: "")
+q14= Question.create(text: "")
+q15= Question.create(text: "")
+q16= Question.create(text: "")
+q17= Question.create(text: "")
+q18= Question.create(text: "")
+q19= Question.create(text: "")
+q20= Question.create(text: "")
+
+q21= Question.create(text: "")
+q22= Question.create(text: "")
+q23= Question.create(text: "")
+q24= Question.create(text: "")
+q25= Question.create(text: "")
+q26= Question.create(text: "")
+q27= Question.create(text: "")
+q28= Question.create(text: "")
+q29= Question.create(text: "")
+q30= Question.create(text: "")
+q31= Question.create(text: "")
+q32= Question.create(text: "")
+q33= Question.create(text: "")
+q34= Question.create(text: "")
+q35= Question.create(text: "")
+q36= Question.create(text: "")
+q37= Question.create(text: "")
+q38= Question.create(text: "")
+q39= Question.create(text: "")
+q40= Question.create(text: "")
+
+q41= Question.create(text: "")
+q42= Question.create(text: "")
+q43= Question.create(text: "")
+q44= Question.create(text: "")
+q45= Question.create(text: "")
+q46= Question.create(text: "")
+q47= Question.create(text: "")
+q48= Question.create(text: "")
+q49= Question.create(text: "")
+q50= Question.create(text: "")
+q51= Question.create(text: "")
+q52= Question.create(text: "")
+q53= Question.create(text: "")
+q54= Question.create(text: "")
+q55= Question.create(text: "")
+q56= Question.create(text: "")
+q57= Question.create(text: "")
+q58= Question.create(text: "")
+q59= Question.create(text: "")
+q60= Question.create(text: "")
+
+answers = Answers.create([
+    {text: "A. JavaScript is a stripped-down version of Java", correct: false, question_id: q1}
+    {text: "B. JavaScript's syntax is loosely based on Java's", correct: true, question_id: q1}
+    {text: "C. They both originated on the island of Java", correct: false, question_id: q1}
+    {text: "D. None of the above", correct: false, question_id: q1}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+    {text: "", correct: , question_id:}
+
+])
+
+
